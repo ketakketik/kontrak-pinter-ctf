cee9c0b15c43f52a037171d5e0526817c1852ac2
Gizka <endolendah4@gmail.com>
2023-05-07 15:51:12 +0700

    hello ethernaut attack

---
+++ src/Ethernaut/01-HelloEthernaut/README.md
@@ -0,0 +1,7 @@
+## Goal
+Memanggil fungsi authenticate dengan parameter yang sama dengan password.
+
+## Clue
+1. function pure
+2. function view
+3. visibility
--- src/Ethernaut/PoC/01-HelloEthernaut.sol
+++ src/Ethernaut/PoC/01-HelloEthernaut.sol
@@ -4,3 +4,26 @@ pragma solidity ^0.8;
 import "../Ethernaut.sol";
 import "../01-HelloEthernaut/HelloEthernautFactory.sol";
 import "forge-std/Test.sol";
+
+contract HelloEthernaut is Test {
+    function test() external {
+        vm.createSelectFork(vm.envString('sepolia'));
+        Ethernaut ethernaut = new Ethernaut();
+        InstanceFactory instance = new InstanceFactory();
+        ethernaut.registerLevel(instance);
+        console.log('setup ethernaut');
+        address alice = makeAddr('alice');
+        vm.startPrank(alice, alice);
+        address helloethernaut = ethernaut.createLevelInstance(instance);
+        Instance hello = Instance(helloethernaut);
+        emit log_named_address('hello', address(hello));
+        console.log('PoC:');
+        console.log('1. check variable password yang bersifat public');
+        emit log_named_string('password', hello.password());
+        console.log('2. masukan password ke parameter fungsi authenticate()');
+        hello.authenticate(hello.password());
+        console.log('submit level');
+        bool sukses = ethernaut.submitLevelInstance(payable(address(hello)));
+        assertTrue(sukses);
+    }
+}
